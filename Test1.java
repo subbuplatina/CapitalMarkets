77a0a3ea26b4f9d5b2fa7f27c5ddcc2a66498672
sasmitalipa <you@sasmitalipa@gmail.com>
2023-03-19 22:24:42 -0400

    added

---
+++ Test1.java
@@ -0,0 +1,10 @@
+package day01;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Friends, Welcome To GIT");
+
+	}
+
+}
---
+++ Test3.java
@@ -0,0 +1,10 @@
+package day01;
+
+public class Test3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("New test");
+	}
+
+}
